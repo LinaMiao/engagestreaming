cd98d8cd67dae372b1864c7067f65378244675bd
Lina Miao <linamiao@pineapple.local>
2017-09-19 09:31:46 -0700

    one ugly file, sort of doing all 7 down streamings, push data to redis

--- spark_streaming/src/main/scala/spark_streaming_comments.scala
+++ spark_streaming/src/main/scala/spark_streaming_comments.scala
@@ -1,3 +1,13 @@
+// multli-stream processing of multiple topics, window aggregation, stateful counting, and sentiment analysis with
+// stanford-corenlp
+// topic_1: comment_topic (window NLP sentiment) -> redis db1 (video_id, avg(sentiment) in sliding window)
+// topic_2: like_topic (window aggregation) -> redis db2 (video_id, count(likes) in sliding window)
+// topic_3: dislike_topic (window_aggregation) -> redis db3 (vide_id, count(dislikes) in sliding window)
+// topic_4: start_topic (bookkeeping streaming/geohash) -> redis db4 (video_id, lat, long, geohash (TODO-FOR RANGE QUERIES)) ** broadcaster event
+// topic_5: end_topic (bookkeeping streaming/geohash) -> redis db5 (video_id, lat, long, geohash (TODO-FOR RANGE QUERIES)) ** broadcaster event
+// topic_6: play_topic (stateful counting, geophashing(TODO)) -> redis db6 (video_id, state_count(start))
+// topic_7: leave_topic (stateful counting, geohasing(TODO)) -> redis db7 (video_id, state_count(leave))
+
 import kafka.serializer.StringDecoder
 
 import org.apache.spark.streaming._
@@ -8,6 +18,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import com.redis.RedisClient
+import org.apache.spark.sql.SQLContext._
 
 
 import java.util.Properties
@@ -20,95 +31,198 @@ import com.databricks.spark.corenlp.functions._
 
 object EngageStreaming {
 
-
-
   def main(args: Array[String]) {
-
-    val brokers = "ec2-54-213-76-65.us-west-2.compute.amazonaws.com:9092"
+    // topics from kafka
+    val brokers = "ec2-34-212-24-253.us-west-2.compute.amazonaws.com:9092"
     val topics_1 = "comments_topic"
     val topicsSet_1 = topics_1.split(",").toSet
     val topics_2 =  "like_topic"
     val topicsSet_2 = topics_2.split(",").toSet
     val topics_3 = "dislike_topic"
-    val topicsSet_3 = topics_1.split(",").toSet
-    val topics_4 =  "video_topic"
-    val topicsSet_4 = topics_2.split(",").toSet
-    val topics_5 =  "watch_topic"
-    val topicsSet_5 = topics_2.split(",").toSet
-
+    val topicsSet_3 = topics_3.split(",").toSet
+    val topics_4 =  "start_topic"
+    val topicsSet_4 = topics_4.split(",").toSet
+    val topics_5 =  "end_topic"
+    val topicsSet_5 = topics_5.split(",").toSet
+    val topics_6 =  "play_topic"
+    val topicsSet_6 = topics_6.split(",").toSet
+    val topics_7 =  "leave_topic"
+    val topicsSet_7 = topics_6.split(",").toSet
 
     // Create context with 10 second batch interval
     val sparkConf = new SparkConf().setAppName("engage")
-    sparkConf.set("spark.streaming.concurrentJobs", "3")
+    sparkConf.set("spark.streaming.concurrentJobs", "7")
 
     val ssc = new StreamingContext(sparkConf, Seconds(10))
     ssc.checkpoint("./tmp")
 
-
     // Create direct kafka stream with brokers and topics
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
 
-
+    // Dstream for topic_1 - comments, with sliding window 30, 30
     val messages_1 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_1)
-    val windowStream_1 = messages_1.window(Seconds(10), Seconds(10))
-
-
-
-
+    val windowStream_1 = messages_1.window(Seconds(30), Seconds(30))
 
     windowStream_1.foreachRDD { rdd =>
 
         val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
         import sqlContext.implicits._
 
-        val lines = rdd.map(_._2)
-        val lines2DF = lines.map(_.split(","))
-                            .map(t => (t(0).toString, t(1).toString)).toDF("comments_id", "comment")
+        val sentiments = rdd.map(_._2)
+                            .map(_.split(","))
+                            .map(t => (t(0).toString()(1).toString(), t(1).toString()))
+                            .toDF("id", "comment")
+                            .select('id, cleanxml('comment).as('doc))
+                            .select('id, explode(ssplit('doc)).as('sen))
+                            .select('id, sentiment('sen).as('sentiment).cast("float")).toDF("id", "sentiment")
+                            .groupBy("id")
+                            .agg(avg("sentiment"))
+        //.select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment)) -> potential extension
 
-        val comment_counts = lines2DF.groupBy("comments_id")
-                .count()
-                .toDF("comments_id","count1")
+        val r1 = new RedisClient("54.201.180.66", 6379, database=1, secret=Option("127001"))
+        sentiments.collect().foreach( t => {r1.set(t(0), t(1).toString())})
+                            }
+
+      // DStream for topic2, likes, with sliding window 10, 10
+      val messages_2 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_2)
+      val windowStream_2 = messages_1.window(Seconds(10), Seconds(10))
+
+      windowStream_2.foreachRDD { rdd =>
+
+          val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+          import sqlContext.implicits._
+
+          val likesCount  = rdd.map(_._2)
+                              .map(_.split(","))
+                              .map(t => (t(0).toString()(1).toString(), 1))
+                              .toDF("id","likes")
+                              .groupBy("id")
+                              .count()
+
+          val r2 = new RedisClient("54.201.180.66", 6379,database=2,secret=Option("127001"))
+          likesCount.collect().foreach( t => {r2.set(t(0), t(1).toString())})
+                              }
+
+      // DStream for topic3, dislikes, with sliding window 10, 10
+      val messages_3 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_3)
+      val windowStream_3 = messages_3.window(Seconds(10), Seconds(10))
+
+      windowStream_3.foreachRDD { rdd =>
+
+          val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+          import sqlContext.implicits._
+
+          val dislikesCount = rdd.map(_._2)
+                              .map(_.split(","))
+                              .map(t => (t(0).toString()(1).toString(), 1))
+                              .toDF("id","dislikes")
+                              .groupBy("id")
+                              .count()
 
 
-        //val sentiments = lines2DF
-        //.select('id, cleanxml('comment).as('doc))
-        //.select('id, explode(ssplit('doc)).as('sen))
-        //.select('id, sentiment('sen).as('sentiment).cast("float")).toDF("id", "sentiment")
+          val r3 = new RedisClient("54.201.180.66", 6379,database=3,secret=Option("127001"))
+          dislikesCount.collect().foreach( t => {r3.set(t(0), t(1).toString())})
+
+                              }
+
+      // DStream for topic4, starts, with sliding window 10, 10
+      val messages_4 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_4)
+      val windowStream_4 = messages_4.window(Seconds(10), Seconds(10))
+
+      windowStream_4.foreachRDD { rdd =>
+
+          val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+          import sqlContext.implicits._
+
+          val starts = rdd.map(_._2)
+                              .map(_.split(","))
+                              .map(t => (t(0).toString()(1).toString(), t(0).toString() + t(1).toString() + t(2).toString() + t(3).toString() + t(4).toString() + t(5).toString() + t(6).toString()))
+                              .toDF("id","content")
 
 
-        //.select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
+
+          val r4 = new RedisClient("54.201.180.66", 6379,database=4,secret=Option("127001"))
+          starts.collect().foreach( t => {r4.set(t(0), t(1))})
+
+                              }
+
+    // DStream for topic5, ends, with sliding window 10, 10
+    val messages_5 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_5)
+    val windowStream_5 = messages_5.window(Seconds(10), Seconds(10))
+
+    windowStream_5.foreachRDD { rdd =>
+
+        val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+        import sqlContext.implicits._
+
+        val endsCount = rdd.map(_._2)
+                            .map(_.split(","))
+                            .map(t => (t(0).toString()(1).toString(), t(0).toString() + t(1).toString() + t(2).toString() + t(3).toString() + t(4).toString() + t(5).toString() + t(6).toString()))
+                            .toDF()
 
 
 
-        //sentiments.groupBy("id")
-        //          .agg(avg("sentiment"))
-        //          .show(5)
-
-        //  .rdd.saveAsTextFile("hdfs://ec2-54-201-180-66.us-west-2.compute.amazonaws.com:9000/user/price_data_output_scala1")
-
-
-
-        val r = new RedisClient("54.201.180.66", 6379, secret=Option("127001"))
-        comment_counts.collect().foreach( t => {r.set(t(0), t(1).toString())})
+        val r5 = new RedisClient("54.201.180.66", 6379,database=5,secret=Option("127001"))
+        endsCount.collect().foreach( t => {r5.set(t(0), t(1))})
 
                             }
 
-      // like_topic
-      val messages_2 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_2)
-      val windowStream_2 = messages_2.window(Seconds(10), Seconds(10))
+      // DStream for topic6, plays, with sliding window 10, 10
+      val messages_6 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_6)
+      val windowStream_6 = messages_6.window(Seconds(10), Seconds(10))
 
-      val initialRDD = ssc.sparkContext.parallelize(List(("0", 0), ("1", 0)))
-      val mappingFunc = (id: String, one: Option[Int], state:State[Int]) => {
+      val initialRDD_6 = ssc.sparkContext.parallelize(List(("0", 0), ("1", 0)))
+      val mappingFunc_6 = (id: String, one: Option[Int], state:State[Int]) => {
         val sum = one.getOrElse(0) + state.getOption.getOrElse(0)
         val output = (id, sum)
         state.update(sum)
         output
       }
 
-      val stateCount = windowStream_2.map(_._2).map(_.split(",")).map(t => (t(0).toString,1)).mapWithState(
-        StateSpec.function(mappingFunc).initialState(initialRDD))
-      stateCount.print()
+      val playStateCount = windowStream_6.map(_._2).map(_.split(",")).map(t => (t(0).toString(),1)).mapWithState(
+        StateSpec.function(mappingFunc_6).initialState(initialRDD_6))
 
+      playStateCount.foreachRDD { rdd =>
+          val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+          import sqlContext.implicits._
+
+          val playCount = rdd.toDF("id","playCount")
+                              .groupBy("id")
+                              .count()
+
+          val r6 = new RedisClient("54.201.180.66", 6379,database=6,secret=Option("127001"))
+          playCount.collect().foreach( t => {r6.set(t(0).toString()(1).toString(), t(1))})
+
+                              }
+
+
+      // DStream for topic7, plays, with sliding window 10, 10
+      val messages_7 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet_7)
+      val windowStream_7 = messages_7.window(Seconds(10), Seconds(10))
+
+      val initialRDD_7 = ssc.sparkContext.parallelize(List(("0", 0), ("1", 0)))
+      val mappingFunc_7 = (id: String, one: Option[Int], state:State[Int]) => {
+        val sum = one.getOrElse(0) + state.getOption.getOrElse(0)
+        val output = (id, sum)
+        state.update(sum)
+        output
+      }
+
+      val leaveStateCount = windowStream_7.map(_._2).map(_.split(",")).map(t => (t(0).toString(),1)).mapWithState(
+        StateSpec.function(mappingFunc_7).initialState(initialRDD_7))
+
+      leaveStateCount.foreachRDD { rdd =>
+          val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+          import sqlContext.implicits._
+
+          val leaveCount = rdd.toDF("id","leaveCount")
+                              .groupBy("id")
+                              .count()
+
+          val r7 = new RedisClient("54.201.180.66", 6379, database=7,secret=Option("127001"))
+          leaveCount.collect().foreach( t => {r7.set(t(0).toString()(1).toString(), t(1))})
+
+                                }
 
 
 
